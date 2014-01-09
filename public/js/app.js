1222f0e61235f8b2a4e9b819994a38a7b5a44fa5
mimouncadosch <mimouncadosch@gmail.com>
2014-01-08 19:37:39 -0500

    initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -8,20 +8,73 @@ angular.module('myApp', [
 	'myApp.filters',
 	'myApp.services',
 	'myApp.directives'
-]).
+	]).
 config(function ($routeProvider, $locationProvider) {
 	$routeProvider.
-		when('/view1', {
-			templateUrl: 'partials/partial1.html',
-			controller: 'MyCtrl1'
-		}).
-		when('/view2', {
-			templateUrl: 'partials/partial2.html',
-			controller: 'MyCtrl2'
-		}).
-			otherwise({
-			redirectTo: '/view1'
-		});
+	when('/view1', {
+		templateUrl: 'partials/partial1.html',
+		controller: 'MyCtrl1'
+	}).
+	when('/view2', {
+		templateUrl: 'partials/partial2.html',
+		controller: 'MyCtrl2'
+	}).
+	otherwise({
+		redirectTo: '/view1'
+	});
 
 	$locationProvider.html5Mode(true);
 });
+
+
+
+function initialize() {
+	var myCenter = new google.maps.LatLng(4.5981,-74.0758);
+	var markersArray = [];
+
+	var mapOptions = {
+		zoom: 15,
+		center: myCenter,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+
+	google.maps.event.addListener(map, 'click', function(event) {
+		placeMarker(event.latLng);
+	});
+
+
+	function placeMarker(location) {
+		deleteOverlays();
+
+		var marker = new google.maps.Marker({
+			map: map,
+			position: location
+		});
+
+		markersArray.push(marker);
+
+		var latitude = location.lat();
+		var longitude = location.lng();
+		var infowindow = new google.maps.InfoWindow({
+			content: "Gracias por reportar"
+		});
+		console.log(latitude);
+		console.log(longitude);
+		infowindow.open(map,marker);
+	}
+
+	function deleteOverlays() {
+		if (markersArray) {
+			for (var i in markersArray) {
+				markersArray[i].setMap(null);
+			}
+			markersArray.length = 0;
+		}
+	}
+	
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,53 @@
  * Serve JSON to our AngularJS client
  */
 
+var mongoose = require('mongoose');
+var queryString = require('querystring');
+var request = require('request');
+
+var mongourl = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/reports'; 
+mongoose.connect(mongourl);
+var db = mongoose.connection;
+
+var reportSchema = mongoose.Schema({
+	createdAt: { type: Date, default: Date.now },
+	title: String,
+	description: String,
+	imageURL: String,
+	neighborhood: String,
+	place: String,
+	time: {type: Date}
+});
+
+var Report = db.model('Report', reportSchema);
+
+
 exports.name = function (req, res) {
   res.json({
   	name: 'Bob'
   });
 };
+
+// Write a function that determines the neighborhood and place based on the latitude and longitude
+// exports.create_crime
+
+exports.generate_crime = function(req, res){
+	Report.create({
+		title: req.body.text,
+		description: req.body.text,
+		imageURL: req.body.text,
+		place: req.body.text,
+		time: req.body.text
+	}, function(err,report) {
+		if (err)
+			res.send(err);
+		Report.find(function(err, reports){
+			if(err)
+				res.send(err);
+			res.json(reports);
+		});
+	});		
+};
+
