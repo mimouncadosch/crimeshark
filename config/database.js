9e2beb770b52d66d75f98b0411a40115ed5a65c0
mimouncadosch <mimouncadosch@gmail.com>
2014-01-28 09:46:30 -0500

    mongo lab works

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 /* hold our database connection settings */ 
 module.exports = {
-	// url looks like: 'mongodb://<host>/<db-name>'
-    'url' : 'mongodb://mimouncadosch:believe@ds027769.mongolab.com:27769/crimeshark' // MongoHQ DB
+    'url' : 'mongodb://crimeshark:columbia18@ds027769.mongolab.com:27769/crimeshark' // MongoHQ DB
 };
