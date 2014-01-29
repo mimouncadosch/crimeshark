bd86fb7367682c9cd525d899eccc1f9adc3f3cab
Gilad Penn <giladpenn@Harrys-MacBook-Pro.local>
2014-01-28 20:51:07 -0500

    user api

--- api/userAPI.js
+++ api/userAPI.js
@@ -34,6 +34,50 @@ module.exports = function(app) {
 			res.json(user.reports);
 		});
 	});
+
+	app.post('/api/updateUser', function (req, res) {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({'_id': id}, function(err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
+
+            // check to see if theres already a user with that email
+            if (user) {
+                // if there is no user with that email
+                // create the user
+                // set the user's local credentials
+                // JSON.parse()
+                user.name = req.param('name');
+                user.phone = req.param('phone');
+                // newUser.contact = JSON.parse(req.param('contact'));
+                user.perimeter = req.param('perimeter');
+
+                // if(perimeter) { 
+                //     var parsedPerimeter = [];
+                //     for (var i = perimeter.length - 1; i >= 0; i--) {
+                //         console.log(perimeter[i]);
+                //         console.log(JSON.parse(perimeter[i]));
+                //         parsedPerimeter.push(JSON.parse(perimeter[i]));
+                //     };
+                //     user.perimeter = parsedPerimeter;
+                //     // newUser.perimeter = JSON.parse(req.param('perimeter')) 
+                // };
+                newUser.local.email     = email;
+                //newUser.hashPassword(password);
+
+                // save the user
+                user.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });        
+
+    });
 };
 
 /////////////
